a5cb13c51152681ba57a2680b2d1f360cde9b486
小樱 <1265578519@qq.com>
2019-06-12 03:42:42 +0800

    修正
    修正

--- 2.94/webgui.sh
+++ 2.94/webgui.sh
@@ -6,4 +6,4 @@ rm -rf /usr/share/transmission/web/index.html
 wget -c http://github.itzmx.com/1265578519/transmission/master/2.94/webgui.zip -O webgui.zip
 unzip -o webgui.zip
 cd webgui
-mv -f * /usr/share/transmission/web/*
+mv -f * /usr/share/transmission/web
