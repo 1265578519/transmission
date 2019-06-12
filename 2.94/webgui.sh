847edec7d0446b8041150491dbaba2af1cfd5e72
小樱 <1265578519@qq.com>
2019-06-12 20:36:01 +0800

    多目录

--- 2.94/webgui.sh
+++ 2.94/webgui.sh
@@ -2,7 +2,7 @@
 yum -y install wget unzip
 wget -c http://github.itzmx.com/1265578519/transmission/master/2.94/index.html -O index.html
 mv -f index.html /usr/share/transmission/web/index.original.html
-rm -rf /usr/share/transmission/web/index.html
+rm -rf /usr/share/transmission/web/index.html /usr/share/transmission/web/index.mobile.html /usr/share/transmission/web/tr-web-control
 wget -c http://github.itzmx.com/1265578519/transmission/master/2.94/webgui.zip -O webgui.zip
 unzip -o webgui.zip
 cd webgui
