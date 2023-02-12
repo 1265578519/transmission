5cb86e3bb2b4dab50ff8f23362c25402f4f8787e
小樱 <1265578519@qq.com>
2023-02-13 02:59:30 +0800

    命令去个d
    命令去个d

--- 4.0.0/transmissionbt.sh
+++ 4.0.0/transmissionbt.sh
@@ -4,7 +4,7 @@ echo "Thanks for using Transmission 4.0.0 for CentOS Auto-Install Script"
 echo "========================================================================="
 setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 yum -y install wget xz gcc gcc-c++ m4 make automake libtool gettext openssl-devel pkgconfig perl-libwww-perl perl-XML-Parser curl curl-devel zlib-devel which libevent cmake iptables-services
-service transmissiond stop 2&> /dev/null
+service transmission stop 2&> /dev/null
 mv -f /home/transmission/Downloads /home
 mv -f /home/transmission/.config/transmission/resume /home
 mv -f /home/transmission/.config/transmission/torrents /home
@@ -27,10 +27,11 @@ make -j 2
 make install
 useradd -m transmission
 passwd -d transmission
-wget http://github.itzmx.com/1265578519/transmission/master/4.0.0/transmission.sh -O /etc/init.d/transmissiond
-chmod 755 /etc/init.d/transmissiond
-chkconfig --add transmissiond
-chkconfig --level 2345 transmissiond on
+ln -s /usr/local/bin/transmission-daemon /usr/bin/transmission-daemon
+wget http://github.itzmx.com/1265578519/transmission/master/4.0.0/transmission.sh -O /etc/init.d/transmission
+chmod 755 /etc/init.d/transmission
+chkconfig --add transmission
+chkconfig --level 2345 transmission on
 mkdir -p /home/transmission/Downloads/
 chmod g+w /home/transmission/Downloads/
 wget -c http://github.itzmx.com/1265578519/transmission/master/4.0.0/settings.json -O settings.json
@@ -39,7 +40,7 @@ mv -f settings.json /home/transmission/.config/transmission/settings.json
 chown -R transmission.transmission /home/transmission
 wget -c http://github.itzmx.com/1265578519/transmission/master/4.0.0/index.html -O index.html
 mv -f index.html /usr/local/share/transmission/public_html/index.html
-service transmissiond start
+service transmission start
 /sbin/iptables -I INPUT -p tcp --dport 9091 -j ACCEPT
 /sbin/iptables -I INPUT -p tcp --dport 22222 -j ACCEPT
 /sbin/iptables -I INPUT -p udp --dport 22222 -j ACCEPT
